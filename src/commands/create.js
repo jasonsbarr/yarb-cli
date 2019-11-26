4c4a6e203affa8f631d00add875d5da43a680990
Jason Barr <jason@jasonsbarr.com>
2019-11-26 00:52:59 -0600

    Create command through cloning template repo for staging

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,7 +1,15 @@
 "use strict";
 const figlet = require("figlet");
 const clear = require("clear");
-const { log, info, notice } = require("../lib/utils/color-logs");
+const ora = require("ora");
+const {
+  log,
+  info,
+  notice,
+  danger
+} = require("../lib/utils/color-logs");
+const { createStagingDir } = require("../lib/modules/staging");
+const { cloneTemplateRepo } = require("../lib/modules/template");
 
 const command = "create <directory>";
 const describe = "Creates new YARB project in <directory>";
@@ -16,15 +24,52 @@ const builder = {
   }
 };
 
-const exec = argv => {
+const exec = async argv => {
+  const projName = argv.name || argv.directory;
+  const initVersion = argv.initial || "1.0.0";
+
   clear();
   notice(figlet.textSync("YARB-CLI"));
-  log(`Creating new project in ${argv.directory}...`);
+  log(
+    `Creating ${projName} with initial version ${initVersion} in ${argv.directory}...`
+  );
+
+  const spinner = ora();
+
+  try {
+    await createStagingDir();
+  } catch (err) {
+    danger(err);
+    throw err;
+  } finally {
+    spinner.stop();
+  }
+
+  log("Getting template repository...");
+
+  spinner.start();
+
+  try {
+    await cloneTemplateRepo();
+  } catch (err) {
+    danger(err);
+    throw err;
+  } finally {
+    spinner.stop();
+  }
 };
 
 const handler = argv => {
   info("Creating new YARB project...");
-  setTimeout(() => exec(argv), 1500);
+  setTimeout(
+    () =>
+      exec(argv)
+        .then(() => {
+          info(figlet.textSync("Project created!"));
+        })
+        .catch(err => console.error(err)),
+    1000
+  );
 };
 
 module.exports = {
