abd18e2b38c5b06c44ebd9dd67e6951898966857
Jason Barr <jason@jasonsbarr.com>
2019-11-25 23:30:35 -0600

    Start making create command

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,4 +1,7 @@
 "use strict";
+const figlet = require("figlet");
+const clear = require("clear");
+const { log, info, notice } = require("../lib/utils/color-logs");
 
 const command = "create <directory>";
 const describe = "Creates new YARB project in <directory>";
@@ -13,7 +16,16 @@ const builder = {
   }
 };
 
-const handler = () => {};
+const exec = argv => {
+  clear();
+  notice(figlet.textSync("YARB-CLI"));
+  log(`Creating new project in ${argv.directory}...`);
+};
+
+const handler = argv => {
+  info("Creating new YARB project...");
+  setTimeout(() => exec(argv), 1500);
+};
 
 module.exports = {
   command,
