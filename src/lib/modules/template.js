bbd6cd6d1e67594924bac08d2300d38bbf791361
Jason Barr <jason@jasonsbarr.com>
2019-11-26 02:13:53 -0600

    Remove simple-git and just use shelljs exec

--- src/lib/modules/template.js
+++ src/lib/modules/template.js
@@ -1,20 +1,15 @@
 const { join } = require("path");
-const simpleGit = require("simple-git");
-const { mv } = require("shelljs");
+const { exec, mv } = require("shelljs");
 
 const templateUrl =
   "https://github.com/jasonsbarr/yet-another-react-boilerplate.git";
 const stagingPath = join(__dirname, "../../../staging/");
 const workingDir = process.cwd();
-let git;
-
-const getGit = () => {
-  git = simpleGit(stagingPath);
-};
 
 const cloneTemplateRepo = async () => {
-  getGit();
-  return await git.clone(templateUrl, "template");
+  return await exec(
+    `git clone ${templateUrl} ${stagingPath}template -v`
+  ).stdout;
 };
 
 const moveTemplateToProjectDir = async dirName => {
