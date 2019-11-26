752388a2b755118b726f1d4420e4610ab2e18325
Jason Barr <jason@jasonsbarr.com>
2019-11-25 22:33:36 -0600

    Yargs parse root command group

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+"use strict";
+
+const { join } = require("path");
+const yargs = require("yargs");
+
+yargs.commandDir(join(__dirname, "lib", "commands")).help().argv;
+
 const run = () => {};
 
 module.exports = run;
