c9fe1bf00bb9b545f819b35121ac0389e52d0a37
Jason Barr <jason@jasonsbarr.com>
2019-11-27 13:36:17 -0600

    Improve help

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const yargs = require("yargs");
 
 const app = yargs
   .commandDir(join(__dirname, "src", "commands"))
+  .alias("v", "version")
+  .alias("h", "help")
+  .usage("Usage: $0 <command> [options]")
+  .usage("Use $0 <command> --help for more info about the command")
   .help();
 
 const run = () => {
